e172eceacfbdb988d4cc09b70c357644e13374dd
dhk1349 <dhk1349@naver.com>
2019-11-01 17:16:24 +0900

    .

--- CPU scheduling/scheduler_module.py
+++ CPU scheduling/scheduler_module.py
@@ -4,6 +4,17 @@ Created on Fri Aug 30 11:58:21 2019
 
 @author: dhk13
 """
+import task_module
+
+def gcd(a,b):
+    """Compute the greatest common divisor of a and b"""
+    while b > 0:
+        a, b = b, a % b
+    return a
+    
+def lcm(a, b):
+    """Compute the lowest common multiple of a and b"""
+    return a * b / gcd(a, b)
 
 class PollingService:
     """
@@ -14,34 +25,85 @@ class PollingService:
 
     Periodic task는 주기가 짧은 task가 우선 순위를 가지고 수행된다.
 
+    Requirement for Execution of this class
+    push Periodic task objects
+    Push Aperiodic task objects
+    Initialize Polling server information
+    
+    Calculate Hyperparameter
+    
+    Then CalTask
+    
+    ***현재 상태: periodic task는 제대로 작동
+    ***작동가능 여부 확인하는 공식 아직 미구현
+    ***aperiodic task 미적
     """
     Name=""
-    PeriodicTask=[]
-    PeriodicExeTime=[]
+    PeriodicTasks=[]
 
-    AperiodicTask=[]
-    AperiodicExeTime=[]
-    AperiodicArrivalTime=[]
+    AperiodicTasks=[]
 
-    PSExeTime=0
-    PSPeriod=0
+    PSExeTime=1 #Polling server exetime
+    PSPeriod=5  #Polling server period, PushTime에 맞게 AP task를 넣어줘야한다.
 
     HyperPeriod=0
     TaskQueue=[]
-    def __init__():
+    def __init__(self):
         self.Name="Polling Service"
 
-    def PushInfo():
-        return 0
-
-    def CalHyperPeriod():
+    def PushPeriodicTask(self,TaskObj):
+        self.PeriodicTasks.append(TaskObj)
+        self.PeriodicTasks.sort(key=lambda object:object.GetPeriod())
+        print (self.PeriodicTasks)
+    
+    def PushAPeriodicTask(self, TaskObj):
+        self.AperiodicTasks.append(TaskObj)
         
-    def CalTask():
-        for i in range(self.HyperPeriod):
+    def CalHyperPeriod(self):
+        HyperPeriod=self.PSPeriod
+        for i in range(len(self.PeriodicTasks)):
+            self.HyperPeriod=lcm(HyperPeriod, self.PeriodicTasks[i].GetPeriod())
+        self.HyperPeriod=int(self.HyperPeriod)
+        print("HyperPEriod is ",self.HyperPeriod)
+        
+    def PollServerTask(self):
+        PSExe=self.PSExeTime
+        for i in range(len(self.AperiodicTasks)):
+            if(self.AperiodicTasks[i].ExeTask(PSExe)==1):
+                print("Executed ",i,"th AP task")
+        if (PSExe==self.PSExeTime):
+            return True
+        return False
+        
+    def CalTask(self):
+        for i in range(self.HyperPeriod):#iterating one cycle of HyperPerid
+            print("Time ",i)
+            if i%self.PSPeriod==0:
+                result=self.PollServerTask()
+                i= i +self. PSExeTime-1 #Just in case Polling server exetime is bigger than 1
+                if result==True:
+                    time=[1]
+                    for i in range(len(self.PeriodicTasks)):
+                        if(self.PeriodicTasks[i].ExeTask(time)==1):
+                            print("Executed ",i,"th Periodic task\n\n")
+            else:
+                time=[1]
+                for i in range(len(self.PeriodicTasks)):
+                    if(self.PeriodicTasks[i].ExeTask(time)==1):
+                        print("Executed ",i,"th Periodic task\n\n")
+        
 
-
-        return 0
 
 class DefferableService:
-
-    def __init__():
+    def __init__(self):
+        self.name="DifferableService"
+        
+if __name__=="__main__":
+    print("Test Execution on scheduler_module.py")
+    test1=PollingService()
+    test1.PushPeriodicTask(task_module.PeriodicTask("Task A",4, 10))
+    test1.PushPeriodicTask(task_module.PeriodicTask("Task B",8, 20))
+    test1.CalHyperPeriod()
+    test1.CalTask()
+    
+    
--- CPU scheduling/task_module.py
+++ CPU scheduling/task_module.py
@@ -8,17 +8,60 @@ Created on Fri Nov  1 00:35:22 2019
 class PeriodicTask:
     ExeTime=-1
     Period=-1
-    def __init__(self, exe, period):
+    def __init__(self, name, exe, period):
+        self.Name=name
         self.ExeTime=exe
         self.Period=period
+        self.TaskQueue=0
         self.Counter=0
+        self.InstanceNum=0
     
     def AddCount(self):
         self.Counter+=1
     
     def AddTask(self):
+        self.TaskQueue+=self.ExeTime
+        if(self.TaskQueue>self.ExeTime):
+            print ("Something Wrogn with Task")
+        self.InstanceNum+=1
         
-    
+    def GetRemainingTask(self):
+        return self.TaskQueue
+        
     def GetPeriod(self):
         return self.Period
     
+    def PrintInfo(self):
+        print ("==========Executed Task==========")
+        print ("Task name: ",self.Name)
+        print ("Instance Number: ",self.InstanceNum)
+        print("Time: ",self.Counter)
+        print("Remaining time: ",self.TaskQueue)
+        print ("=================================")
+    
+    def ExeTask(self, exe):
+        if(self.Counter%self.GetPeriod()==0):
+            self.AddTask()
+        
+        if (exe[0]>0 and self.TaskQueue>0):
+            self.TaskQueue-=1
+            exe[0]-=1
+            self.PrintInfo()
+            self.Counter+=1
+            return 1
+        self.Counter+=1
+        return 0
+
+class APeriodicTask:
+    def __init__(self, exe):
+        self.TaskQueue=exe
+        
+    def GetRemainingTask(self):
+        return self.Exetime
+    
+    def ExeTask(self, exe):
+        if (exe>0 and self.TaskQueue>0):
+            self.TaskQueue-=1
+            exe-=1
+            return 1
+        return 0
