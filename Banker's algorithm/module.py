88828f9b8528bd4bd8309fea3cfcfe2a4f6e1e81
dhk1349 <41980618+dhk1349@users.noreply.github.com>
2019-11-22 02:08:28 +0900

    Update module.py

--- Banker's algorithm/module.py
+++ Banker's algorithm/module.py
@@ -5,3 +5,15 @@ Created on Fri Nov 22 00:40:24 2019
 @author: dhk1349
 """
 
+"""
+성공적인 경로를 찾는 방법은 recursive한 함수를 통해서 구현한다. 
+정의할 것은
+
+자원의 종류와 갯수
+프로세스의 갯수
+프로세스의  Allocation, Max, Available, Need
+
+들어오는 값은 특정 프로세스 x가 추가적인 자원의 요청 값 
+  
+""" 
+
