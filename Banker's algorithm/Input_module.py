deb1a5e59bb08556691f12f0efbe89a5a841a48a
Donghoon Han <dhk1349@naver.com>
2019-11-28 17:40:19 +0900

    update

--- Banker's algorithm/Input_module.py
+++ Banker's algorithm/Input_module.py
@@ -4,31 +4,54 @@ Created on Wed Nov 27 20:27:49 2019
 
 @author: dhk1349
 """
-
-def Check(Resourcelst, processmaxlst, processresourcelst):
-    #Things need to be checked 
-    
-
-
-
+import random
+import math
+def parser(inputlst):
+    result=[]
+    for i in range(0,len(inputlst),2):
+        result.append(int(inputlst[i]))
+    return result
+        
 def InputMode():
     ResourcesNum=int(input("Enter Number of Resources: "))
     InstanceNum=input("Enter numbers of Instance for each resource: ")
-    ProcessNum=int(input("Enter Number of Processes"))
+    ProcessNum=int(input("Enter Number of Processes: "))
     
     ProcessResourceList=[]
     ProcessMaxList=[]
     
     print("**********Setting Max Resource**********")
     for i in range(ProcessNum):
-        temp=input(str(i+1)+". Set Max Resources to Process")
-        ProcessMaxList.append(temp)        
+        temp=input(str(i+1)+". Set Max Resources to Process: ")
+        ProcessMaxList.append(parser(temp))        
     print("*****************************************\n")
     
     print("**********Allocating Resources**********")
     for i in range(ProcessNum):
-        temp=input(str(i+1)+". Allocate Resources to Process")
-        ProcessResourceList.append(temp)
+        temp=input(str(i+1)+". Allocate Resources to Process: ")
+        ProcessResourceList.append(parser(temp))
     print("*****************************************\n")
     
+    print("result")
+    print (ResourcesNum)
+    print (InstanceNum)
+    print (ProcessNum)
+    print (ProcessResourceList)
+    print (ProcessMaxList)
 
+def RandomeInputGenerator():
+    #number of Resources: 3-100
+    #number of Instances: ?
+    #number of processes: 
+    #number of Max resources for each process
+    #number of request resources for each process
+    number=random.randrange(1, pow(3, 97)+1)
+    print(100-math.ceil(math.log(number,3)))
+    ResourcesNum=number
+    InstanceNum
+    ProcessNum
+    ProcessMaxList
+    ProcessResourcList
+    
+#InputMode()
+RandomeInputGenerator()
