6ff2dcb61288dfadfef43f4c52477db0c12bd07c
Donghoon Han <dhk1349@naver.com>
2019-12-03 02:07:25 +0900

    Update module.py

--- Contiguous Allocation Memory Management/module.py
+++ Contiguous Allocation Memory Management/module.py
@@ -9,9 +9,28 @@ Created on Sun Dec  1 03:06:28 2019
 Requirment:
     -input: Randomly generated input file
     -Allocate process to space where it best fits.
-    -
+    -Coalescing
+    -Compaction (Brute force case and efficient case each)
 """
 class Process:
-    
+    def __init__(self, initializer):
+        self.base=initializer[0]
+        self.end=initializer[1]
+        self.limit=initializer[1]-initializer[0]
 
-class 
+class EmptySpace:
+    def __init__(self, initializer):
+        self.base=initializer[0]
+        self.end=initializer[1]
+        self.limit=initializer[1]-initializer[0]
+        
+class Memory:
+    def __init__(self, initializer):
+        self.base=initializer[0]
+        self.end=initializer[1]
+        
+    def Coalescing(self):
+        return 
+    
+    def Compaction(self):   
+        return
