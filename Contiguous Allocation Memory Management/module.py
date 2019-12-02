dfcfccf406259e88b23bcd52eac278d197910349
Donghoon Han <dhk1349@naver.com>
2019-12-02 21:06:00 +0900

    Update module.py

--- Contiguous Allocation Memory Management/module.py
+++ Contiguous Allocation Memory Management/module.py
@@ -4,5 +4,14 @@
 Created on Sun Dec  1 03:06:28 2019
 
 @author: donghoon
-"""
+@Project Name: Contiguous Allocation Memory Mangement
 
+Requirment:
+    -input: Randomly generated input file
+    -Allocate process to space where it best fits.
+    -
+"""
+class Process:
+    
+
+class 
