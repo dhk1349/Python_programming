5cad9bb0adc23dcd4294697db77faf8fa282523a
Donghoon Han <dhk1349@naver.com>
2019-12-05 01:25:36 +0900

    update

--- Contiguous Allocation Memory Management/module.py
+++ Contiguous Allocation Memory Management/module.py
@@ -25,14 +25,37 @@ class Process:
     
     def GetEnd(self):
         return self.end
+    def PrintInfo(self):
+        print("===========")
+        print(self.name)
+        print(self.base)
+        print(self.end)
+        print("===========")
 
+        
 class EmptySpace:
     def __init__(self, initializer):
         self.base=initializer[0]
         self.end=initializer[1]
         self.limit=initializer[1]-initializer[0]
-        self.name=initializer[2]
+        self.name=str(initializer[2])
         self.type="Empty Space"
+        
+    def GetBase(self):
+        return self.base
+    def GetEnd(self):
+        return self.end
+    def GetLimit(self):
+        return self.limit
+    def GetName(self):
+        return self.name
+    
+    def PrintInfo(self):
+        print("===========")
+        print(self.name)
+        print(self.base)
+        print(self.end)
+        print("===========")
         
 class Memory:
     """
@@ -44,19 +67,148 @@ class Memory:
         self.base=initializer[0]
         self.end=initializer[1]
         self.ProcessList=[]
+        self.EmptyList=[]
+        
+    def SearchEmptyObject(self,name):
+        for i in self.EmptyList:
+            if name==i.GetName():
+                return i
+        return 0
     
-    def Iterator(self):
+    def SearchProcess(self,name):
+        for i in self.ProcessList:
+            if name==i.GetName():
+                return i
+        return 0
+            
+    def InitializeEmptySpace(self):
         #initialize empty space after operation
         Base=self.base
+        index=0
+        self.EmptyList=[]
+        
         for i in self.ProcessList:
             if(i.GetBase()-Base>0):
-                
-            
+                self.EmptyList.append(EmptySpace([Base, i.GetBase(), "Empty-"+str(index)]))
+                index+=1
+                Base=i.GetEnd()
+            elif(i.GetBase()-Base<0):
+                print ("Illegal Allocation of Memory")
+                self.EmptyList=[]
+                return 0
+        if len(self.ProcessList)==0:
+            self.EmptyList.append(EmptySpace([self.base, self.end, "Empty Base"]))
     
+            
+    def AddProcess(self, data):
+        """
+        data:    
+            [size, name]
+        Add Process with Best Fit
+        """
+        bestmin=-1
+        bestindex=-1
+        for i in self.EmptyList:
+            if(i.GetLimit()>(data[0])):
+                #process can fit into this space
+                if bestmin==-1:
+                    print("1")
+                    bestmin=i.GetLimit()-data[0]
+                    bestindex=i.GetName()
+                elif bestmin>(i.GetLimit()-data[0]):
+                    print("2")
+                    #best fits so far
+                    bestmin=i.GetLimit()-data[0]
+                    bestindex=i.GetName()
+                else:
+                    print("NONE of them above")
+        if bestindex==-1:
+            print("Illegal situation")
+            return 0
+        else:
+            EmptyObj=self.SearchEmptyObject(bestindex)
+            Base=EmptyObj.GetBase()
+            for i in range(len(self.ProcessList)):
+                if self.ProcessList[i].GetBase()>Base:
+                    #initialize process
+                    print("Process Added")
+                    self.ProcessList.insert(i, Process([Base, Base+data[0], data[1]]))
+                    break
+            if(len(self.ProcessList)==0):
+                self.ProcessList.append(Process([Base, Base+data[0],"Process-1"+data[1]]))
+            
+    def DeleteProcess(self, data):
+        """
+        data:
+            name
+        """
+        process=self.SearchProcess(data)
+        if process!=0:
+            self.ProcessList.remove(process)
+            return 1
+        return 0
+            
     def Coalescing(self):
+        print ("Coalescing")
+        """
+        InitializeEmptySpace함수가 이미 이 역학을 해주는 듯 하다.
+        """
         return 
     
-    def Compaction(self):   
+    def Compaction(self): 
+        print("Compaction")
         return
     
+    def PrintStatus(self):
+        """
+        이건 나중에,,
+        while True:
+            EmptyIndex=0
+            ProcessIndex=0
+            
+            
+            if(EmptyIndex==len(self.ProcessList) and ProcessIndex==len(self.ProcessList)):
+                break
+        """
+        for i in self.EmptyList:
+            i.PrintInfo()
+        for i in self.ProcessList:
+            i.PrintInfo()
+
+class Manager:
+    def __init__(self):
+        self.Memory=Memory([0, 255])
     
+    def Execute(self):
+        fd=open('input.txt','r')
+        data=fd.readline()
+        data=data.split()
+        #print(data)
+        
+        for i in range(0, len(data), 2):
+            if int(data[i+1])==0:
+                #erase process
+                self.Memory.InitializeEmptySpace()
+                self.Memory.DeleteProcess(data[i])
+            else:
+                #add Process
+                self.Memory.InitializeEmptySpace()
+                self.Memory.AddProcess([int(data[i+1]), data[i]])
+        self.Memory.InitializeEmptySpace()
+
+        self.Memory.PrintStatus()
+if __name__=="__main__":
+    a=Manager()
+    a.Execute()
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
