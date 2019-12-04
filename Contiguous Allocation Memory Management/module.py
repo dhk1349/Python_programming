a4e8c359fcff574ec10d791b00ca6eae1dcd6e4c
Donghoon Han <dhk1349@naver.com>
2019-12-04 23:15:28 +0900

    Update module.py

--- Contiguous Allocation Memory Management/module.py
+++ Contiguous Allocation Memory Management/module.py
@@ -17,20 +17,46 @@ class Process:
         self.base=initializer[0]
         self.end=initializer[1]
         self.limit=initializer[1]-initializer[0]
+        self.name=initializer[2]
+        self.type="Process"
+    
+    def GetBase(self):
+        return self.base
+    
+    def GetEnd(self):
+        return self.end
 
 class EmptySpace:
     def __init__(self, initializer):
         self.base=initializer[0]
         self.end=initializer[1]
         self.limit=initializer[1]-initializer[0]
+        self.name=initializer[2]
+        self.type="Empty Space"
         
 class Memory:
+    """
+    Operation:
+        Add process 
+        Delete process
+    """
     def __init__(self, initializer):
         self.base=initializer[0]
         self.end=initializer[1]
-        
+        self.ProcessList=[]
+    
+    def Iterator(self):
+        #initialize empty space after operation
+        Base=self.base
+        for i in self.ProcessList:
+            if(i.GetBase()-Base>0):
+                
+            
+    
     def Coalescing(self):
         return 
     
     def Compaction(self):   
         return
+    
+    
