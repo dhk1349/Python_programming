6d5073755babf20a219e11d99f9c8d4e96d3fce6
dhk1349 <41980618+dhk1349@users.noreply.github.com>
2020-01-18 23:03:30 +0900

    Create join practice.py

---
+++ Random codes/join practice.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 18 23:00:36 2020
+
+@author: dhk1349
+"""
+
+sentense="Life is short so we need python"
+
+container=sentense.split()
+print(" ".join(container))
