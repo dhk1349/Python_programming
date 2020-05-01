b5770642a95fd1322efeb43984823d423b0b93b5
dhk1349 <41980618+dhk1349@users.noreply.github.com>
2020-05-01 21:51:54 +0900

    Update SQL_practice2.sql

--- SQL_practice2.sql
+++ SQL_practice2.sql
@@ -26,4 +26,27 @@ WHERE (salary between 5000 and 12000) and (department_id=20 or department_id=50)
 
 SELECT last_name, hire_date
 FROM employees
-WHERE hire_date>='1994-01-01' and  hire_date<='1994-12-31';
+WHERE hire_date>='1994-01-01' and  hire_date<'1995-01-01';
+
+SELECT last_name, job_id
+FROM employees
+WHERE manager_id is NULL;
+
+SELECT last_name, salary, commission_pct
+FROM employees
+WHERE commission_pct IS NOT NULL
+order by salary, commission_pct;
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE '__a%';
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE '%a%' and last_name LIKE '%e%';
+
+SELECT last_name, job_id, salary
+FROM employees
+WHERE job_id IN ('SA_REP', 'ST_CLERK') and salary NOT IN (2500, 3500, 7000);
+
+
