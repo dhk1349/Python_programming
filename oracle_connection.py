3122a53367c2b35c50ac66fbcd78f399d05c8d53
dhk1349 <41980618+dhk1349@users.noreply.github.com>
2020-06-03 15:18:19 +0900

    .

--- oracle_connection.py
+++ oracle_connection.py
@@ -12,7 +12,7 @@ import os
 os.putenv('NLS_LANG', '.UTF8')
 
 #연결에 필요한 기본 정보 (유저, 비밀번호, 데이터베이스 서버 주소)
-connection = cx_Oracle.connect('admin','ehdgns4232','lolgg.culykwl5fmgv.ap-northeast-2.rds.amazonaws.com/orcl')
+connection = cx_Oracle.connect('admin',)
 
 cursor = connection.cursor()
 
