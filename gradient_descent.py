16301b1874d7bdaf363f1a7f96f922e519daa084
dhk1349 <dhk1349@naver.com>
2020-10-01 00:48:26 +0900

    Update gradient_descent.py

--- gradient_descent.py
+++ gradient_descent.py
@@ -6,6 +6,9 @@ Created on Wed Sep 30 21:08:34 2020
 """
 import numpy as np
 
+def func(x):
+    return 2*x[0]**2 + 3*x[0]*x[1] + 2*x[1]**2 -4*x[0]+2*x[1]-24
+
 def partial_1(x):
     return 4*x[0]+3*x[1]-4
 
@@ -14,14 +17,22 @@ def partial_2(x):
 
 x0=np.array([1,0.9])
 
+min_set=[22/7, -20/7]
+
+
 iter_list=x0
 
 g=np.array([])
 lr=0.1
 
+print("min= ", func(min_set))
 
-for _ in range(100):
-    g=(partial_1(iter_list)+partial_2(iter_list))/2
-    print(g)
-    iter_list=iter_list-lr*g
+for _ in range(10):
+    g1=partial_1(iter_list)
+    g2=partial_2(iter_list)
+    print(g1, g2)
+    iter_list[0]=iter_list[0]-lr*g1
+    iter_list[1]=iter_list[1]-lr*g2
+    
     print(iter_list)
+    print("value= ", func(iter_list))
